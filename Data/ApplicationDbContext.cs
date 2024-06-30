4e08acd66919b69663a7a27483155bb4a00b3a2f
Uchennaore <oreoluwa371@gmail.com>
2024-06-30 23:43:22 +0100

    updated Dbcontext and added to startup file

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using smart_credit.Entities;
 using Smart_Credit.Entities;
 
 namespace Smart_Credit.Data
@@ -10,6 +11,15 @@ namespace Smart_Credit.Data
         }
 
         public DbSet<Deadline> DeadlineDates { get; set; }
+        public DbSet<Intermediary> Intermediaries { get; set; } 
+        public DbSet<Lender> Lenders { get; set; }  
+        public DbSet<Loan> Loans { get; set; }  
+        public DbSet<Repayment> Repayments { get; set; }
+        public DbSet<Borrower> Borrowers { get; set; }  
+        public DbSet<Addressee> Addressees { get; set; }    
+        public DbSet<LenderBorrower> LenderBorrowers { get; set; }  
+        public DbSet<LoanRequestLender> LoanRequestLenders { get; set; }    
+        public DbSet<LoanRequest> LoanRequests { get; set; }
         
     }
 }
--- Entities/Borrower.cs
+++ Entities/Borrower.cs
@@ -6,6 +6,8 @@ namespace Smart_Credit.Entities;
 public class Borrower
 {
 	public int Id { get; set; }	
+    public string firstName { get; set; }
+    public string lastName { get; set; }    
 	public int AddresseeId { get; set; }
     [ForeignKey("AddresseeId")]
     public Addressee Addressee { get; set; }
